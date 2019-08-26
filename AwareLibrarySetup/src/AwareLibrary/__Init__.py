4f581be5752905e4786911f1bde10419d8b11a4e
Chalermkiat Phafongyun <chalermkiat.p@aware.co.th>
2019-08-26 14:15:57 +0700

    Update AwareLibary - Add Keyword Document

--- AwareLibrarySetup/src/AwareLibrary/__Init__.py
+++ AwareLibrarySetup/src/AwareLibrary/__Init__.py
@@ -8,6 +8,30 @@ class AwareLibrary(Face, LeftHand):
         LeftHand.__init__(self)
 
     def get_hello_world(self):
+        """Verifies that element ``locator`` is found on the current page.
+
+        See the `Locating elements` section for details about the locator
+        syntax.
+
+        The ``message`` argument can be used to override the default error
+        message.
+
+        The ``limit`` argument can used to define how many elements the
+        page should contain. When ``limit`` is ``None`` (default) page can
+        contain one or more elements. When limit is a number, page must
+        contain same number of elements.
+
+        See `Page Should Contain` for explanation about the ``loglevel``
+        argument.
+
+        Examples assumes that locator matches to two elements.
+        | `Page Should Contain Element` | div_name | limit=1    | # Keyword fails.                  |
+        | `Page Should Contain Element` | div_name | limit=2    | # Keyword passes.                 |
+        | `Page Should Contain Element` | div_name | limit=none | # None is considered one or more. |
+        | `Page Should Contain Element` | div_name |            | # Same as above.                  |
+
+        The ``limit`` argument is new in SeleniumLibrary 3.0.
+        """
         return "Helloworld"
 
     def get_method_void(self):
